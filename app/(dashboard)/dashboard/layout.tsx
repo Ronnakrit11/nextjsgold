f3b16112e7433595038b8bcb487afc63e459405e
21 Tech Enterprise <59superrichsol@gmail.com>
2025-01-13 11:36:35 +0700

    hide customer

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -82,7 +82,7 @@ export default function DashboardLayout({
           </nav>
         </aside>
 
-        {/* Main content */}
+        {/* Main contednt */}
         <main className="flex-1 overflow-y-auto p-0 lg:p-4">{children}</main>
       </div>
     </div>
