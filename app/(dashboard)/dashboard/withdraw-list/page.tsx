8faaf6aa54c7fa71d70dcc03eb8e2f45e8f503bf
21 Tech Enterprise <59superrichsol@gmail.com>
2025-01-19 21:19:19 +0700

    done all

--- app/(dashboard)/dashboard/withdraw-list/page.tsx
+++ app/(dashboard)/dashboard/withdraw-list/page.tsx
@@ -97,7 +97,7 @@ export default function WithdrawListPage() {
   return (
     <section className="flex-1 p-4 lg:p-8">
       <h1 className="text-lg lg:text-2xl font-medium text-gray-900 mb-6">
-        Withdrawal Requests
+        ประวัติการถอนทองทั้งหมด
       </h1>
       <Card>
         <CardHeader>
--- app/(dashboard)/dashboard/withdraw-money-requests/page.tsx
+++ app/(dashboard)/dashboard/withdraw-money-requests/page.tsx
@@ -99,7 +99,7 @@ export default function WithdrawMoneyRequestsPage() {
   return (
     <section className="flex-1 p-4 lg:p-8">
       <h1 className="text-lg lg:text-2xl font-medium text-gray-900 mb-6">
-        Withdrawal Requests
+        ประวัติการขอถอนเงินทั้งหมด
       </h1>
 
       <Card>
